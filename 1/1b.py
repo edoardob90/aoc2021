53b909f7c53025cac835201d341639cd60e1eb10
edoardob90 <edoardob90@gmail.com>
2021-12-06 23:16:46 +0100

    A few improvements to moving_map

--- 1/1b.py
+++ 1/1b.py
@@ -2,10 +2,12 @@ import fileinput
 
 depths = [int(line.strip()) for line in fileinput.input()]
 
-def moving_map(func, data, size):
+def moving_map(func, data, size=1):
     """Applies `func` over a window of size `size` in the specified `data`"""
-    new_data = [data[i:i-size] for i in range(size)] + [data[size:]]
-    assert len(new_data) == size+1
+    assert size > 0 and size < len(data), "Invalid window size"
+    M = len(data) - size - 1
+    new_data = [data[i:i - M] for i in range(M)] + [data[M:]]
+    assert len(new_data) == M + 1
     return list(map(func, zip(*new_data)))
 
 def subtract(data):
