c1b76fc741b827db0b3cf76cefefe91496fdb211
edoardob90 <edoardob90@gmail.com>
2021-12-15 22:17:51 +0100

    GitHub Actions test

---
+++ .github/scripts/update_calendar.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+day=$(date +%d)
+title=$(curl -s "https://adventofcode.com/2021/day/$day" | grep -o "<h2>.*</h2>" | sed -E "s/<h2>--- Day.*: (.*) ---<\/h2>/\1/g")
+echo ".. | $day: $title ([puzzle text](https://adventofcode.com/2021/day/$day)) | | |"
