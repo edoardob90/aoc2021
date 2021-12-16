8da4e40fc3c17721ec4765b06de533952c7e95b6
edoardob90 <edoardob90@gmail.com>
2021-12-16 16:21:48 +0100

    Update Actions main.yml

--- .github/scripts/update_calendar.sh
+++ .github/scripts/update_calendar.sh
@@ -2,4 +2,4 @@
 
 day=$(date +%d)
 title=$(curl -s "https://adventofcode.com/2021/day/$day" | grep -o "<h2>.*</h2>" | sed -E "s/<h2>--- Day.*: (.*) ---<\/h2>/\1/g")
-echo ".. | $day: $title ([puzzle text](https://adventofcode.com/2021/day/$day)) | | |"
+echo ".. | $day: $title ([puzzle text](https://adventofcode.com/2021/day/$day)) | | |" | tee -a README.md
